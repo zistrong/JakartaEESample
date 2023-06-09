ddb547e14cb847df4987d044201187276ace2c96
zistrong <zistrong@163.com>
2023-06-09 10:05:28 +0800

    websocket: chatroom

--- src/main/java/com/zistrong/jakartaee/websocket/ChatRoom.java
+++ src/main/java/com/zistrong/jakartaee/websocket/ChatRoom.java
@@ -4,39 +4,49 @@ package com.zistrong.jakartaee.websocket;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-@ServerEndpoint("/chatroom/{roomId}/{nickName}")
+@ServerEndpoint("/chatroom/{roomId}")
 public class ChatRoom {
+    private final Logger logger = LoggerFactory.getLogger(ChatRoom.class);
+
+    private final static Map<String, Session> sessionMap = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(Session session, @PathParam("roomId") long roomId,
-                       @PathParam("nickName") String unDecName) {
-        System.out.println(roomId);
+    public void onOpen(Session session, @PathParam("roomId") String roomId) {
+        logger.info(roomId);
+        sessionMap.putIfAbsent(roomId, session);
     }
 
     @OnMessage
-    public void onMessage(Session session, String message, @PathParam("roomId") long roomId,
-                          @PathParam("nickName") String unDecName) {
+    public void onMessage(Session session, String message, @PathParam("roomId") String roomId) {
 
-        System.out.println(message);
-        try {
-            session.getBasicRemote().sendText(session.getId() + ":" + message + new Date());
-        } catch (IOException e) {
-        }
+        sessionMap.forEach((k, v) -> {
+            if(!k.endsWith(roomId)) {
+                try {
+                    v.getBasicRemote().sendText("hello,message, I'm " + message);
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+        });
     }
 
     @OnClose
-    public void onClose(Session session, @PathParam("roomId") long roomId,
-                        @PathParam("nickName") String unDecName) {
+    public void onClose(Session session, @PathParam("roomId") String roomId) throws IOException {
 
-        System.out.println(roomId + "/" + unDecName);
+        logger.info("close ="+roomId);
+        session.close();
+        sessionMap.remove(roomId);
     }
 
     @OnError
-    public void onError(@PathParam("uname") String uname, Session session, Throwable error) {
+    public void onError(Session session, Throwable error) {
         error.printStackTrace();
     }
 
